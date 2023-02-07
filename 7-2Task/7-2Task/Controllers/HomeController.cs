e777f8628c60a37fec4c43ad92fd10dca01c237b
AmerAmora <Amer.m.amora@gmail.com>
2023-02-07 15:17:34 +0300

    7-2 Tasks

---
+++ 7-2Task/7-2Task/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using _7_2Task.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _7_2Task.Controllers
+{
+    public class HomeController : Controller
+    {
+        Task3Entities db= new Task3Entities();
+        public ActionResult Index()
+        {
+            var emp = db.employees.ToList();
+            return View(emp);
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 7-2Task/7-2Task/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7_2Task.MvcApplication" Language="C#" %>
