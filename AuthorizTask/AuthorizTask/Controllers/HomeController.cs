604164e4598e5fba22de98eed722e022aa2475c5
unknown <amer.m.amora@gmail.com>
2023-02-12 01:04:08 +0300

    Upload

---
+++ AuthorizTask/AuthorizTask/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AuthorizTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizTask.Controllers
+{
+    [Authorize]   
+    
+    public class HomeController : Controller
+    {
+        Task3Entities db = new Task3Entities();
+        public ActionResult Index()
+        {
+            var emp = db.employees.ToList();
+            return View(emp);
+        }
+
+        [Authorize(Roles="Admin")]
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AuthorizTask/AuthorizTask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuthorizTask.Startup))]
+namespace AuthorizTask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
