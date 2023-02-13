c5365630ea1ad6a433b9fc579e95434b619a5cd4
AmerAmora <Amer.m.amora@gmail.com>
2023-02-13 23:49:38 +0300

    update

---
+++ TaskApi/TaskApi/Controllers/MajorsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskApi.Models;
+
+namespace TaskApi.Controllers
+{
+    public class MajorsController : Controller
+    {
+        private ApiTaskEntities db = new ApiTaskEntities();
+
+        // GET: Majors
+        public ActionResult Index()
+        {
+            var majors = db.Majors.Include(m => m.Faculty);
+            return View(majors.ToList());
+        }
+        public ActionResult Major(int? id)
+        {
+            var majors = db.Majors.Where(a => a.Faculity_id == id).Include(a => a.Faculty);
+            return View(majors.ToList());
+        }
+
+        // GET: Majors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Major major = db.Majors.Find(id);
+            if (major == null)
+            {
+                return HttpNotFound();
+            }
+            return View(major);
+        }
+
+        // GET: Majors/Create
+        public ActionResult Create()
+        {
+            ViewBag.Faculity_id = new SelectList(db.Faculties, "Faculity_id", "Faculity_Name");
+            return View();
+        }
+
+        // POST: Majors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Major_id,Major_Name,Faculity_id")] Major major)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Majors.Add(major);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Faculity_id = new SelectList(db.Faculties, "Faculity_id", "Faculity_Name", major.Faculity_id);
+            return View(major);
+        }
+
+        // GET: Majors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Major major = db.Majors.Find(id);
+            if (major == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Faculity_id = new SelectList(db.Faculties, "Faculity_id", "Faculity_Name", major.Faculity_id);
+            return View(major);
+        }
+
+        // POST: Majors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Major_id,Major_Name,Faculity_id")] Major major)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(major).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Faculity_id = new SelectList(db.Faculties, "Faculity_id", "Faculity_Name", major.Faculity_id);
+            return View(major);
+        }
+
+        // GET: Majors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Major major = db.Majors.Find(id);
+            if (major == null)
+            {
+                return HttpNotFound();
+            }
+            return View(major);
+        }
+
+        // POST: Majors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Major major = db.Majors.Find(id);
+            db.Majors.Remove(major);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
